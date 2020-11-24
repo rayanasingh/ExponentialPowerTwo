9d2c487dd05b3084a12099e0c54c92f0f8fe6c6b
Rayna Singh <rayanasingh8@gmail.com>
2020-11-24 10:52:35 +0100

    Initial commit

---
+++ src/com/exponentialpower/ExponentialPowerTwo.java
@@ -0,0 +1,9 @@
+package com.exponentialpower;
+import java.lang.Math;
+
+public class ExponentialPowerTwo {
+    public static void main(String[] args) {
+        int ans = (int)Math.pow(5,2);
+        System.out.println(+ans);
+    }
+}
